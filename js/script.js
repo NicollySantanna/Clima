92daabfd4f07c63d3e1bec2e8868f2642f852325
Nicolly <nicollyengenheira@gmail.com>
2023-04-12 13:03:09 -0300

    Acrescentando funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Variáveis e seleção de elementos
 const apiKey = "dee0a27dca1793d266d59786e4aced61";
-const apiCountryUrl = "https://flagsapi.com/${}/flat/64.png";
+// const apiCountryUrl = "https://flagsapi.com/BR/flat/64.png";
 
 const cityInput = document.querySelector("#city-input")
 const searchBtn = document.querySelector("#search")
@@ -10,31 +10,42 @@ const tempElement = document.querySelector("#temperature span")
 const descElement = document.querySelector("#description")
 const weatherIconElement = document.querySelector("#weather-icon")
 const countryElement = document.querySelector("#country")
-const umidityElement = document.querySelector("#umidity span")
+const humidityElement = document.querySelector("#humidity span")
 const windElement = document.querySelector("#wind span")
 
+const weatherContainer = document.querySelector("#weather-data")
+
 // Funções
-const getWeatherData = async(city) =>{
+const getWeatherData = async (city) => {
     const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
 
     const res = await fetch(apiWeatherUrl)
     const data = await res.json()
 
-    console.log(data)
-}
+    return data;
+};
 
+const showWeatherData = async (city) => {
+   const data = await getWeatherData(city);
 
-const showWeatherDta = (city) => {
-getWeatherData(city)
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    // countryElement.setAttribute("src", apiCountryUrl + data.sys.country)
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`
+
+    weatherContainer.classList.remove("hide")
 }
 
 
 
 // Eventos
-searchBtn.addEventListener("click", (e) =>{
+searchBtn.addEventListener("click", (e) => {
 e.preventDefault()
 
 const city = cityInput.value
 
-console.log(city)
+showWeatherData(city)
 })
